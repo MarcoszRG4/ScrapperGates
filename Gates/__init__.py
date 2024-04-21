46e25cd05a9240fe037abef0a0fc0789dc8127a9
MarcoszRG4 <167573004+MarcoszRG4@users.noreply.github.com>
2024-04-21 01:08:04 -0500

    Add files via upload

---
+++ Gates/__init__.py
@@ -0,0 +1,4 @@
+#Aqui vas agregando los gates que quieras usando el import que hace referencia a la funcion del gate, por ejemplo:
+from Gates.Auths.b3auth import braintree_auth6x
+from Gates.Charged.shcyb import ShopiGate
+from Gates.Charged.payflowavs import PayflowAVS
